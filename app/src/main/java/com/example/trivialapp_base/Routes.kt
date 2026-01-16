1e4d509b0d51a19d7268d2b5104f0e2774b638bd
unai.arevalo.7e8 <unai.arevalo.7e8@itb.cat>
2026-01-16 09:49:09 +0100

    feat: Add Navigation Controller and View Model Theme.kt function name changed to "TrivialApp_NAHU"

--- app/src/main/java/com/example/trivialapp_base/Routes.kt
+++ app/src/main/java/com/example/trivialapp_base/Routes.kt
@@ -1,5 +1,8 @@
 package com.example.trivialapp_base
 
 sealed class Routes(val route: String) {
-
+    object LaunchScreen: Routes("Splash")
+    object MenuScreen: Routes("Menu")
+    object GameScreen: Routes("Game")
+    object ResultScreen: Routes("Result")
 }
