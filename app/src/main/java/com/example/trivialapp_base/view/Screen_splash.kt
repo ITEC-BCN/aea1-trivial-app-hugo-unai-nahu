afb9502f0cd9f9c45a25457dc3055faa0c6af78b
hugoasencio7 <hugo.asencio.7e9@itb.cat>
2026-01-21 08:42:55 +0100

    Add basic splash screen

--- app/src/main/java/com/example/trivialapp_base/view/Screen_splash.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_splash.kt
@@ -1,9 +1,48 @@
 package com.example.trivialapp_base.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import kotlinx.coroutines.delay
+import com.example.trivialapp_base.R
+import com.example.trivialapp_base.Routes
 
 @Composable
 fun SplashScreen(navController: NavController) {
+    LaunchedEffect(Unit) {
+        delay(3000)
+        navController.navigate(Routes.MenuScreen.route) {
+            popUpTo(Routes.LaunchScreen.route) { inclusive = true }
+        }
+    }
 
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.images),
+            contentDescription = "Logo del Trivial",
+            modifier = Modifier.size(200.dp)
+        )
+
+        Text(
+            text = "¡Trivial NAHU",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
 }
