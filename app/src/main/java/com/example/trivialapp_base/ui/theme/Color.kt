a07bb1e9ec7da481142d4ac16acddb019b96b80c
unai.arevalo.7e8 <unai.arevalo.7e8@itb.cat>
2026-01-22 10:03:54 +0100

    feat: Game Screen stile modified. He, Vegetta. He, Vegetta. Eres el mejor youtuber.

--- app/src/main/java/com/example/trivialapp_base/ui/theme/Color.kt
+++ app/src/main/java/com/example/trivialapp_base/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+//New Colors
+val PurpleBack = Color(0xFFEFC8F7)
+val PurpleFront = Color(0xFFB641FF)
+val Purple777 = Color(0xFF883EA9)
