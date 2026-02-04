fcfb20735d671d77baf5799805b658da91e002d9
unai.arevalo.7e8 <unai.arevalo.7e8@itb.cat>
2026-02-04 10:09:04 +0100

    feat: The game screen stile was modified

---
+++ app/src/main/java/com/example/trivialapp_base/ui/theme/Shape.kt
@@ -0,0 +1,7 @@
+package com.example.trivialapp_base.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+
+val cardShape = RoundedCornerShape(24.dp)
--- app/src/main/java/com/example/trivialapp_base/view/Screen_game.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_game.kt
@@ -5,61 +5,61 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
 import com.example.trivialapp_base.R
 import com.example.trivialapp_base.ui.theme.Purple777
 import com.example.trivialapp_base.ui.theme.PurpleBack
-import com.example.trivialapp_base.ui.theme.PurpleFront
-import com.example.trivialapp_base.ui.theme.PurpleGrey80
+import com.example.trivialapp_base.ui.theme.cardShape
+import com.example.trivialapp_base.ui.theme.lilaAccent
+import com.example.trivialapp_base.ui.theme.lilaFondo
 import com.example.trivialapp_base.viewmodel.GameViewModel
 
 @Composable
 fun GameScreen(navController: NavController, viewModel: GameViewModel) {
-    Image(
-        painter = painterResource(id = R.drawable.gamebackground),
-        contentDescription = "Background",
-        contentScale = ContentScale.Fit,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(PurpleGrey80)
-            .padding(horizontal = 10.dp)
-    )
     ConstraintLayout(
         modifier = Modifier
             .fillMaxSize()
+            .background(lilaFondo)
     ) {
-        val (currentPoints, timeBar, image, answersBox, question, firstAnswer, secondAnswer, thirdAnswer, fourthAnswer, correctAnswer) = createRefs()
+        val (currentPoints, timeBar, image, answersBox, question, answer) = createRefs()
 
         Box(
             modifier = Modifier
-                .clip(RoundedCornerShape(20.dp))
+                .clip(cardShape)
                 .background(PurpleBack)
-                .border(5.dp, Purple777, RoundedCornerShape(20.dp))
-                .padding(20.dp)
+                .border(5.dp, lilaAccent, cardShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp)
                 .constrainAs(currentPoints) {
                     top.linkTo(parent.top, margin = 50.dp)
                     end.linkTo(parent.end, margin = 50.dp)
                 }
         ){
-            Text("${viewModel.points} pts.")
+            Text("${viewModel.points} pts.",
+                fontWeight = FontWeight.Bold
+            )
         }
 
         LinearProgressIndicator(
@@ -67,16 +67,16 @@ fun GameScreen(navController: NavController, viewModel: GameViewModel) {
         modifier = Modifier
                         .padding(25.dp, 10.dp)
                         .height(25.dp)
-                        .border(5.dp, Purple777, RoundedCornerShape(20.dp))
+                        .border(5.dp, lilaAccent, cardShape)
                         .width(500.dp)
-                        .clip(RoundedCornerShape(20.dp))
+                        .clip(cardShape)
                         .constrainAs(timeBar) {
                             top.linkTo(currentPoints.bottom)
                             bottom.linkTo(image.top)
                             start.linkTo(parent.start)
                             end.linkTo(parent.end)
                         },
-        color = PurpleFront,
+        color = Purple777,
         trackColor = PurpleBack,
         strokeCap = StrokeCap.Butt,
         )
@@ -87,11 +87,11 @@ fun GameScreen(navController: NavController, viewModel: GameViewModel) {
             contentScale = ContentScale.Fit,
             modifier = Modifier
                 .size(200.dp)
-                .border(10.dp, Purple777, RoundedCornerShape(20.dp))
-                .clip(RoundedCornerShape(20.dp))
+                .border(10.dp, lilaAccent, cardShape)
+                .clip(cardShape)
                 .constrainAs(image) {
                     top.linkTo(parent.top)
-                    bottom.linkTo(parent.bottom, margin = 120.dp)
+                    bottom.linkTo(parent.bottom, margin = 250.dp)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
                 }
@@ -99,133 +99,97 @@ fun GameScreen(navController: NavController, viewModel: GameViewModel) {
 
         Box(
             modifier = Modifier
-                .clip(RoundedCornerShape(20.dp))
-                .background(PurpleFront)
-                .border(5.dp, Purple777, RoundedCornerShape(20.dp))
+                .clip(cardShape)
+                .background(Color.White)
+                .border(5.dp, lilaAccent, cardShape)
                 .padding(20.dp)
                 .constrainAs(question) {
-                    top.linkTo(image.bottom, margin = 40.dp)
+                    top.linkTo(image.bottom, margin = 20.dp)
                     start.linkTo(image.start)
                     end.linkTo(image.end)
                 }
         ){
-            Text(viewModel.currentQuestion.question, textAlign = TextAlign.Center)
+            Text(
+                text = viewModel.currentQuestion.question,
+                textAlign = TextAlign.Center,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Medium
+            )
         }
 
-        Box(
-            modifier = Modifier
-                .clip(RoundedCornerShape(20.dp))
-                .size(height = 130.dp, width = 230.dp)
-                .background(PurpleBack)
-                .border(5.dp, Purple777, RoundedCornerShape(20.dp))
-                .constrainAs(answersBox) {
-                    top.linkTo(question.bottom, margin = 20.dp)
-                    start.linkTo(question.start)
-                    end.linkTo(question.end)
+        Column (
+            modifier = Modifier.constrainAs(answersBox) {
+                top.linkTo(question.bottom)
+                start.linkTo(question.start)
+                end.linkTo(question.end)
+            }
+        ) {
+            viewModel.mixedAnswers.forEachIndexed { index, answer ->
+                AnswerButton(
+                    text = answer,
+                    color = lilaAccent
+                ) {
+                    if (!viewModel.questionAnswered) viewModel.answerQuestion(viewModel.mixedAnswers[index])
                 }
-        )
-        Box(
-            modifier = Modifier
-                .size(height = 50.dp, width = 100.dp)
-                .clickable {
-                    if (!viewModel.questionAnswered) viewModel.answerQuestion(
-                        viewModel.mixedAnswers[0]
-                    )
-                }
-                .clip(RoundedCornerShape(20.dp))
-                .background(PurpleFront)
-                .padding(10.dp)
-                .clip(RoundedCornerShape(10.dp))
-                .constrainAs(firstAnswer) {
-                    top.linkTo(answersBox.top, margin = 10.dp)
-                    start.linkTo(answersBox.start, margin = 10.dp)
-                }
-        ){ Text(viewModel.mixedAnswers[0], Modifier.align(
-            alignment = Alignment.Center
-        )) }
-        Box(
-            modifier = Modifier
-                .size(height = 50.dp, width = 100.dp)
-                .clickable {
-                    if (!viewModel.questionAnswered) viewModel.answerQuestion(
-                        viewModel.mixedAnswers[1]
-                    )
-                }
-                .clip(RoundedCornerShape(20.dp))
-                .background(PurpleFront)
-                .padding(10.dp)
-                .clip(RoundedCornerShape(10.dp))
-                .constrainAs(secondAnswer) {
-                    top.linkTo(answersBox.top, margin = 10.dp)
-                    end.linkTo(answersBox.end, margin = 10.dp)
-                    start.linkTo(firstAnswer.end, margin = 10.dp)
-                }
-        ){ Text(viewModel.mixedAnswers[1], Modifier.align(
-            alignment = Alignment.Center
-        )) }
-        Box(
-            modifier = Modifier
-                .size(height = 50.dp, width = 100.dp)
-                .clickable {
-                    if (!viewModel.questionAnswered) viewModel.answerQuestion(
-                        viewModel.mixedAnswers[2]
-                    )
-                }
-                .clip(RoundedCornerShape(20.dp))
-                .background(PurpleFront)
-                .padding(10.dp)
-                .clip(RoundedCornerShape(10.dp))
-                .constrainAs(thirdAnswer) {
-                    top.linkTo(firstAnswer.bottom, margin = 10.dp)
-                    start.linkTo(answersBox.start, margin = 10.dp)
-                    bottom.linkTo(answersBox.bottom, margin = 10.dp)
-                }
-        ){ Text(viewModel.mixedAnswers[2], Modifier.align(
-            alignment = Alignment.Center
-        )) }
-        Box(
-            modifier = Modifier
-                .size(height = 50.dp, width = 100.dp)
-                .clickable {
-                    if (!viewModel.questionAnswered) viewModel.answerQuestion(
-                        viewModel.mixedAnswers[3]
-                    )
-                }
-                .clip(RoundedCornerShape(20.dp))
-                .background(PurpleFront)
-                .padding(10.dp)
-                .clip(RoundedCornerShape(10.dp))
-                .constrainAs(fourthAnswer) {
-                    top.linkTo(secondAnswer.bottom, margin = 10.dp)
-                    start.linkTo(thirdAnswer.end, margin = 10.dp)
-                    end.linkTo(answersBox.end, margin = 10.dp)
-                    bottom.linkTo(answersBox.bottom, margin = 10.dp)
-                }
-        ){ Text(viewModel.mixedAnswers[3], Modifier.align(
-            alignment = Alignment.Center
-        )) }
-
-        if (viewModel.questionAnswered){
+            }
+        }
+        if (viewModel.questionAnswered) {
             Box(
                 modifier = Modifier
-                    .clip(RoundedCornerShape(20.dp))
-                    .background(PurpleBack)
-                    .border(5.dp, Purple777, RoundedCornerShape(20.dp))
+                    .clip(cardShape)
+                    .background(Color.White)
                     .padding(20.dp)
-                    .constrainAs(correctAnswer) {
-                        top.linkTo(answersBox.bottom, margin = 30.dp)
-                        start.linkTo(answersBox.start)
-                        end.linkTo(answersBox.end)
+                    .constrainAs(answer){
+                        top.linkTo(answersBox.bottom)
+                        start.linkTo(question.start)
+                        end.linkTo(question.end)
                     }
-            ){
+            ) {
                 Text(
-                    "The correct answer was...\n${viewModel.currentQuestion.correctAnswer}\nPress any where to continue...",
-                    textAlign =  TextAlign.Center
+                    text = "Respuesta correcta:\n${viewModel.currentQuestion.correctAnswer}",
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold
                 )
             }
-            Box( modifier = Modifier.fillMaxSize().clickable{ viewModel.continueRound() } )
-        }
 
-        if (viewModel.gameFinalized) navController.navigate("Result")
+            Box(
+                modifier = Modifier.fillMaxSize()
+                    .clickable(onClick = {
+
+                        if (viewModel.gameFinalized) {
+                            navController.navigate("Result")
+                        }
+                        else{
+                            viewModel.continueRound()
+                        }
+                    })
+            )
+        }
+    }
+}
+
+@Composable
+private fun AnswerButton(
+    text: String,
+    color: Color,
+    onClick: () -> Unit
+) {
+    Spacer(Modifier.size(10.dp))
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(0.8f)
+            .clip(cardShape)
+            .background(color)
+            .clickable { onClick() }
+            .padding(vertical = 14.dp, horizontal = 16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            color = Color.White,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
     }
 }
